0a07d906045f20acc2297ba9f2e6cfe8d9f8dd5d
Renan Araújo <renan-ano10@hotmail.com>
2018-03-03 19:36:24 -0300

    Classe Documento

---
+++ classDocumento.php
@@ -0,0 +1,17 @@
+<?php
+
+class Documento {
+    private $nomero;
+
+    public function getNumero(){
+        return $this->numero;
+    }
+    public function setNumero($numero){
+        $this->numero = $numero;
+    }
+
+    $cpf = new Documento();
+    $cpf->setNumero();
+}
+
+?>
