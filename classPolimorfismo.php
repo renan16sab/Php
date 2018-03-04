409a08f8cbf394341158d60e13b34fd75606c09a
Renan Araújo <renan-ano10@hotmail.com>
2018-03-04 12:58:11 -0300

    Classes Polimorfismo

---
+++ classPolimorfismo.php
@@ -0,0 +1,53 @@
+<?php
+
+abstract class Animal {
+    public function falar(){
+        return "som";
+    }
+
+    public function mover(){
+        return "Anda";
+    }
+}
+
+class Gato extends Animal {
+
+    public function falar(){
+        return "Mia";
+    }
+}
+
+class Cachorro extends Animal {
+    public function falar(){
+        return "Late";
+    }
+}
+
+class Passaro extends Animal {
+    public function falar(){
+        return "Canta";
+    }
+
+    public function mover(){
+        return "Voa" . parent::mover();
+    }
+}
+
+$gato = new Gato();
+echo $gato->falar();
+echo $gato->mover();
+
+echo "<br/>";
+
+$cachorro = new Cachorro();
+echo $cachorro->falar();
+echo $cachorro->mover();
+
+echo "<br/>";
+
+$passaro = new Passaro();
+echo $passaro->falar();
+echo $passaro->mover();
+
+
+
